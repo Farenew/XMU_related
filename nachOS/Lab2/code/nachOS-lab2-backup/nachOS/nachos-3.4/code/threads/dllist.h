016dde62b898fcd56c4e64c1cc0ab6f529832002
ForenewHan <jiang-feng@outlook.com>
2018-05-08 12:00:29 +0800

    started on Lab2

---
+++ nachOS/Lab2/code/nachOS-lab2-backup/nachOS/nachos-3.4/code/threads/dllist.h
@@ -0,0 +1,44 @@
+//
+// Created by jiang on 2018/3/24.
+//
+
+#ifndef NACHOS_DLLIST_H
+#define NACHOS_DLLIST_H
+
+#define NULL 0
+
+class DLLElement{
+public:
+    DLLElement(void *itemPtr, int sortKey);
+    DLLElement *next;
+    DLLElement *prev;
+    int key;
+    void *item;
+};
+
+class DLList{
+    friend int findKey(DLLElement* first, DLLElement *last, int mark);
+public:
+    DLList();
+    ~DLList();
+
+    void Prepend(void *item);
+    void Append(void *item);
+    void *Remove(int *keyPtr);
+
+    bool isEmpty();
+
+    void SortedInsert(void *item, int sortKey);
+    void *SortedRemove(int sortKey);
+
+private:
+    DLLElement *first;
+    DLLElement *last;
+};
+
+
+void genItems(DLList *dl, const int N);
+void remove(DLList *dl, const int N);
+
+
+#endif 
